fc187f6fbb8f5e6fab136d08a2d96de028b966f2
Sergio <saggamedeveloper@gmail.com>
2017-11-17 16:18:44 +0100

    Created initialMenu.js

---
+++ src/js/Scenes/initialMenu.js
@@ -0,0 +1,19 @@
+//level1.js
+ç
+var defaultScene = require('./defaultScene.js');
+
+var level1 = {
+  preload: function(){
+    
+  },
+
+  create: function(){
+
+  },
+
+  update: function(){
+
+  }
+};
+
+module.exports = level1;
