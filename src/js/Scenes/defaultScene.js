a63f6774f2e36faaf6e7da464ae91f59fd86cf1a
Sergio <saggamedeveloper@gmail.com>
2017-11-17 01:21:44 +0100

    Improved arquitecture

--- src/js/Scenes/defaultScene.js
+++ src/js/Scenes/defaultScene.js
@@ -4,14 +4,26 @@ var pit = require('../Entities/Characters/pit.js');
 
 var defaultScene = {
   myPit: undefined,
+  entities: [],
+
+  createSprite: function(name, x, y, game){
+    var sprite = game.add.sprite(x, y, name);
+    sprite.anchor.setTo(0, 0);
+
+    return sprite;
+  },
 
   preload: function(){
-    myPit = new pit();
+    this.game.load.spritesheet('pit', '../../images/characters/pit.png', 45, 45, 26);
   },
 
   create: function(){
-    myPit.create();
-  }
+    var pitSprite = defaultScene.createSprite('pit', 1, 1, this.game);
+    myPit = pit(1, 1, pitSprite);
+    myPit.newAnimation('walk', [21, 22, 23, 24], 5, true, true);
+  },
+
+  update: function(){}
 };
 
 module.exports = defaultScene;
