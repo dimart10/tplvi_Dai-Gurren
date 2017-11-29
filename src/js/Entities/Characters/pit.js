f9de43b93927c1ee22ad665f905a2139201b7568
Diego <dimart10@ucm.es>
2017-11-29 18:39:47 +0100

    Pit now can jump
    Basic jump behaviour with two possible lengths depending on how long the spacebar bar is pressed

--- src/js/Entities/Characters/pit.js
+++ src/js/Entities/Characters/pit.js
@@ -17,7 +17,13 @@ function pit(game, x, y, name){
   this.newAnimation('walkRight', [10, 9, 8, 7], 15, true, false);
   this.newAnimation('walkLeft', [3, 4, 5, 6], 15, true, false);
 
+
+  this.spacebar = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
   this.cursors = game.input.keyboard.createCursorKeys(); //TESTING
+
+  //PROVISIONAL
+  this.game = game;
+  this.jumptimer=0;
 }
 
 pit.prototype = Object.create(entity.prototype); //Inherits from entity
@@ -29,6 +35,7 @@ pit.prototype.newAnimation = function (name, frames, fps, repeat, playOnCreate){
 
 pit.prototype.update = function(){
   this.move();
+  this.jump();
 }
 
 pit.prototype.move = function(){ //TESTING
@@ -46,6 +53,60 @@ pit.prototype.move = function(){ //TESTING
         if (this.animations.name == "walkRight") this.animations.play("stillRight");
         else if (this.animations.name == "walkLeft") this.animations.play("stillLeft");
       }
+}
+
+
+pit.prototype.jump = function(){
+
+  if(this.spacebar.isDown && this.checkIfCanJump()) {
+    this.jumptimer=1;
+    this.body.velocity.y = -150;
+  }
+
+  else if(this.spacebar.isDown && (this.jumptimer!=0))  {
+      if(this.jumptimer > 40){
+        this.jumptimer=0;
+      }
+      else{
+        this.jumptimer++;
+        this.body.velocity.y=-150;
+      }
+    }
+
+  else if (this.jumptimer != 0){
+    this.jumptimer=0;
+  }
+}
+
+
+
+
+
+
+
+//Method that checks if the player is touching the ground
+pit.prototype.checkIfCanJump = function() {
+  var c, d, i, result, yAxis;
+  yAxis = p2.vec2.fromValues(0, 1);
+  result = false; //returned variable, initialized as false
+  //Loop that iterates through the collisions in the game
+  for (i=0; i < this.game.physics.p2.world.narrowphase.contactEquations.length; i++ ) {
+    //Variable c contains the collisions in the i position of the vector
+    c = this.game.physics.p2.world.narrowphase.contactEquations[i];
+    //If a collisions includes this.body as either of the bodies colliding
+    if (c.bodyA === this.body.data || c.bodyB === this.body.data) {
+      //D is assigned a position to check if this.body is above or below
+      //the other body (this comment needs more work)
+      d = p2.vec2.dot(c.normalA, yAxis);
+      if (c.bodyA === this.body.data) {
+        d *= -1;
+      }
+      if (d > 0.5) {
+        result = true;
+      }
+    }
+  }
+  return result;
 }
 
 module.exports = pit;
